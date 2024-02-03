b6a84b1e9f7d2103e25ae9c37f09220ff3c2771b
Faheem Akhtar <faheem.akhtar@unthinkable.co>
2024-02-04 01:16:35 +0530

    Fresh Fruits sections

--- src/app/shared/fruits-lists/fruits-lists.component.ts
+++ src/app/shared/fruits-lists/fruits-lists.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
-
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { NgForOf } from '@angular/common';
 @Component({
   selector: 'app-fruits-lists',
   standalone: true,
-  imports: [],
+  imports: [NgForOf],
   templateUrl: './fruits-lists.component.html',
   styleUrl: './fruits-lists.component.css'
 })
 export class FruitsListsComponent {
+  @Input() contents: Array<{ image: string; alt: string; text: string; heading:string }> = [];
+  @Output() clicked: EventEmitter<void> = new EventEmitter<void>();
+  onClick(): void {
+    this.clicked.emit();
+  }
+  onButtonClick(content: { image: string; alt: string; text: string }): void {
+    console.log('Button clicked for:', content);
+  }
 
 }
