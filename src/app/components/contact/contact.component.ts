533569f1da7ab08f669edd778b0b9f1b26ed220f
Faheem Akhtar <faheem.akhtar@unthinkable.co>
2024-02-04 22:35:03 +0530

    child component practices

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
-
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
 export class ContactComponent {
+  @Output() formSubmit: EventEmitter<any> = new EventEmitter<any>();
+  contactForm: FormGroup;
+  constructor(private fb: FormBuilder) {
+    this.contactForm = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      message: ['', Validators.required],
+    });
+  }
 
+  onSubmit() {
+    if (this.contactForm.valid) {
+      // Emit the form data to the parent component
+      this.formSubmit.emit(this.contactForm.value);
+      // Optionally, you can reset the form after submission
+      this.contactForm.reset();
+    }
+  }
 }
